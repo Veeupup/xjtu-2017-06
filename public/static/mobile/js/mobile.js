200dae7043f13aecf257d4036e4832925c30eb1b
tw2016xjtu <931418134@qq.com>
2017-07-16 22:35:11 +0800

    移动端：去掉一开始页面显示的提示页面

--- public/static/mobile/js/mobile.js
+++ public/static/mobile/js/mobile.js
@@ -44,31 +44,5 @@
         fingers: 'all'
     });
 
-    /* only for index */
-    (function () {
-        var isHideHelp = false;
-        var listener = function () {
-            if ($('.main-content-index').length) {
-                if (isHideHelp) {
-                    $('.main-content-index-help').hide();
-                } else {
-                    $('.main-content-index-help').click(function () {
-                        $(this).hide();
-                        isHideHelp = true;
-                    });
-                }
-                $('.main-content-index-swiper').height(window.innerHeight - $('.header').height());
-                var swiper = new Swiper('.main-content-index-swiper', {
-                    pagination: '.main-content-index-swiper .swiper-pagination',
-                    paginationClickable: true,
-                    direction: 'vertical'
-                });
-                $('.footer').hide();
-            } else {
-                $('.footer').show();
-            }
-        };
-        $(document).on('pjax:end', listener);
-        listener();
-    })();
+    
 })();
